3cc1a5ded24e82d327145708d1d0b68140907b5e
Relooper <shra1w@gmail.com>
2022-06-30 19:08:52 +0530

    added progress dialog

--- app/src/main/java/com/s1studios/shra1slibrary/ShraX.kt
+++ app/src/main/java/com/s1studios/shra1slibrary/ShraX.kt
@@ -1,11 +1,15 @@
 package com.s1studios.shra1slibrary
 
 import android.app.Application
+import android.app.ProgressDialog
+import android.content.Context
 import android.util.Log
 import android.widget.Toast
 
 object ShraX {
     var application: Application? = null
+
+    var progressDialog: ProgressDialog? = null
 
     fun toast(text: String) {
         Toast.makeText(ShraX.application, text, Toast.LENGTH_SHORT).show()
@@ -19,4 +23,14 @@ object ShraX {
         Log.d("ShraX", "[${clazz.simpleName}] $text")
     }
 
+    fun pleaseWaitDialog(context: Context) {
+        progressDialog = ProgressDialog(context)
+        progressDialog?.setTitle("Please wait...")
+        progressDialog?.setCancelable(false)
+        progressDialog?.show()
+    }
+
+    fun dismissProgressDialog(){
+        progressDialog?.dismiss()
+    }
 }
